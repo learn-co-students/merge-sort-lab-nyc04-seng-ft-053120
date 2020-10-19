ec409b02377b883b73c7139b7450f58151b28570
vuonga1103 <vuonga1103@yahoo.com>
2020-10-19 16:40:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function findMinAndRemoveSorted(sortedArray) {
+  return sortedArray.splice(0, 1)[0]
+}
+
+function merge(firstSubarray, secondSubarray) {
+  let result = []
+
+  // while BOTH of the arrays have something in them
+  while (firstSubarray.length && secondSubarray.length) {
+
+    // Comparing the first elem of firstSubarr with first elem of secondSubarray
+    if (firstSubarray[0] < secondSubarray[0]) {
+      result.push(findMinAndRemoveSorted(firstSubarray))
+    } else {
+      result.push(findMinAndRemoveSorted(secondSubarray))
+    }
+  }
+  return [...result, ...firstSubarray, ...secondSubarray]
+}
+
+
+function mergeSort(unsortedArr) {
+  // base case
+  if (unsortedArr.length === 1) return unsortedArr;
+
+  let midpoint = Math.floor(unsortedArr.length / 2);
+  let firstHalf = unsortedArr.slice(0, midpoint);
+  let secondHalf = unsortedArr.slice(midpoint);
+
+  return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+}
 
