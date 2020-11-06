410e33715fa18b626984c30a6ea30f11d3f67429
mariamoljames <mariamol.james@gmail.com>
2020-11-06 18:15:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-
+function findMinAndRemoveSorted(arr){
+    return arr.shift()
+    }
+    function merge(arr1,arr2){
+        let sortedArr=[]
+        while(arr1.length>0 && arr2.length>0){
+                if(arr1[0]<arr2[0]){
+                    sortedArr.push(findMinAndRemoveSorted(arr1))
+                }else{
+                    sortedArr.push(findMinAndRemoveSorted(arr2))
+                }
+            }
+            return sortedArr.concat(arr1).concat(arr2)       
+    }
+    function mergeSort(arr){  
+      let midpoint = arr.length/2
+      let firstHalf = arr.slice(0,midpoint)
+      let secondHalf = arr.slice(midpoint, arr.length)
+      let sorted
+      if(arr.length<2){
+          return arr
+      }else{
+           sorted = merge(mergeSort(firstHalf),mergeSort(secondHalf))
+      }
+      return sorted
+    }
